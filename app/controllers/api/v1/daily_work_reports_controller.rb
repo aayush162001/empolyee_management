ab3ed905901d6c01465855699a560e19b1ff3926
Aayush Gandhi <aayushgandhi162001@gmail.com>
2023-07-25 16:09:44 +0530

    auth-work

--- app/controllers/api/v1/daily_work_reports_controller.rb
+++ app/controllers/api/v1/daily_work_reports_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::DailyWorkReportsController < ApplicationController
     before_action :authenticate_user!
     load_and_authorize_resource
     def index
-        daily_work_reports = DailyWorkReport.all
+        daily_work_reports = DailyWorkReport.accessible_by(current_ability)
         render json: daily_work_reports
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_20_043038) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_25_100140) do
   create_table "attendances", force: :cascade do |t|
     t.integer "user_id", null: false
     t.date "attendance_date"
@@ -79,6 +79,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_20_043038) do
     t.string "contact"
     t.integer "role", default: 0
     t.integer "department", default: 0
+    t.string "authentication_token"
+    t.index ["authentication_token"], name: "index_users_on_authentication_token"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
