8f0220bddef3b20c486af330f7f761aae252495d
Aayush Gandhi <aayushgandhi162001@gmail.com>
2023-08-03 17:28:42 +0530

    Rake-Task

--- app/models/check_in_out.rb
+++ app/models/check_in_out.rb
@@ -1,44 +1,19 @@
 class CheckInOut < ApplicationRecord
   belongs_to :user
   validate :unique_check_in, :on => :create
-  
-  
   def calculate_attendance
 
-      # binding.pry
+    # binding.pry
 
-      a = CheckInOut.where(attendance_date:Date.today).where(user_id:user.id)
-      b = ((Time.current - a.last.check_in)/3600).round(2)
-      self.work_hours = b
+    a = CheckInOut.where(attendance_date:Date.today).where(user_id:user.id)
+    b = ((Time.current - a.last.check_in)/3600).round(2)
+    self.work_hours = b
 
   end
 
   def hours_sum
     # binding.pry
-    sum_of_hours = CheckInOut.where(user_i    # binding.pry
-      sum_of_hours = CheckInOut.where(user_id:user.id).where(attendance_date:Date.today).sum("work_hours")
-      if sum_of_hours >= 8.00
-          if Attendance.exists?(user_id:user.id,attendance_date: Date.today)
-              if DailyWorkReport.exists?(user_id: user.id, current_date: Date.today)
-                  x = user.attendances.where(attendance_date:Date.today)
-                  # binding.pry
-                  c = Attendance.find(x.pluck(:id).first)
-                  # c = a.find{|data| data[:present] == nil }
-                  # a.update(c[:present] = 1)
-  #               person = Person.find(2)
-                  c.update({present: true})
-              end
-  .            else
-              Attendance.create(user_id: user.id, attendance_date: Date.today,present:0)
-          end
-      end
-      # c = 0 
-      #   a.all.each do |b|
-      #     c += ((b.check_out )-(b.check_in))/3600
-      #   end
-  
-      # self.present = user.daily_work_reports.exists?(user_id: user_id, current_date: attendance_date)
-      d:user.id).where(attendance_date:Date.today).sum("work_hours")
+    sum_of_hours = CheckInOut.where(user_id:user.id).where(attendance_date:Date.today).sum("work_hours")
     if sum_of_hours >= 8.00
         if Attendance.exists?(user_id:user.id,attendance_date: Date.today)
             if DailyWorkReport.exists?(user_id: user.id, current_date: Date.today)
